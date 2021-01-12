d4c478fc1edc4ae56f1e0f80daf4b0d7eae71201
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2021-01-12 16:54:27 -0500

    Update BigFixUsersList.php

--- BigFixUsersList.php
+++ BigFixUsersList.php
@@ -1,22 +1,19 @@
 <?php
+// This query returns a list BigFix Users that the current BigFix User is able to access
 	// Defines output as an XML Document
 	header('Content-type: application/xml');
 	
 	// Fetches HTTP variables from the PHP's Domain URL into PHP variables
-	$userName = $_GET['user'];
-	$password = $_GET['pass'];
-	$server = $_GET['serv'];
+	$userName = $_GET['user'];   // BigFix Username
+	$password = $_GET['pass'];   // BigFix Password
+	$server = $_GET['serv'];     // BigFix Server Name Ex:"bigfixserver:52311"
 	
 	// Relevance Query as Concatenated String
 	$relevance = 
 		'( '.
-			//'id of it, '.
-			//'name of it, '.
-			//'"'.$server.'" '.
 			'id of item 0 of it, '.
 			'name of item 0 of it, '.
 			'(substring after "https://" of it) of url of item 1 of it as string '.
-			//'(substring before ":42311" of (substring after "https://" of it)) of url of item 1 of it as string'.
 		') of '.
 		'( '.
 			'bes users'.
@@ -24,7 +21,7 @@
 		')';
 	
 	// HTTP Encoding to make the relevance query URL Friendly
-	$relevance = str_replace('%', '%252525', $relevance);
+	$relevance = str_replace('%', '%252525', $relevance); // This is the reason why I could not simply use "urlencode".  In order for '%' to be used in the relevance as a string you must specially encode it like this.
 	$relevance = str_replace(' ', '%20', $relevance);
 	$relevance = str_replace('!', '%21', $relevance);
 	$relevance = str_replace('"', '%22', $relevance);
