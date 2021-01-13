99a7200c3dd42cf935b629c2b294796acfbd8311
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2021-01-13 06:36:26 -0500

    Update UserAccessDetails.php

--- UserAccessDetails.php
+++ UserAccessDetails.php
@@ -1,12 +1,14 @@
 <?php
+// 
+	
 	// Defines output as an XML Document
 	header('Content-type: application/xml');
 	
 	// Fetches HTTP variables from the PHP's Domain URL into PHP variables
-	$userName = $_GET['user'];
-	$password = $_GET['pass'];
-	$server = $_GET['serv'];
-	$filter = $_GET['cg'];
+	$userName = $_GET['user'];   // BigFix Username
+	$password = $_GET['pass'];   // BigFix Password
+	$server = $_GET['serv'];     // BigFix Server Name  EX:"bigfixserver.companyname.com:52311"
+	$filter = $_GET['cg'];       // Computer Group Name, case sensitive
 	
 	$filter = str_replace('%20', ' ', $filter);
 	$filter = str_replace('%2F', '/', $filter);
@@ -17,7 +19,6 @@
 		'( '.
 			'id of item 0 of it, '.
 			'(name of item 0 of it | "<none>") & " <br>" & (device type of item 0 of it | "<none>"), '.
-			//'device type of item 0 of it | "<none>", '.
 			'concatenation " <br>" of values of results from (bes property "User Name") of item 0 of it as string | "<none>", '.
 			'((preceding text of first " " of operating system of item 0 of it) as string & " <br>" & (following text of first " " of operating system of item 0 of it) as string) | "<none>", '.
 			'concatenation " <br>" of (ip addresses of item 0 of it as string) | "<none>", '.
@@ -124,7 +125,7 @@
 		') ';
 	
 	// HTTP Encoding to make the relevance query URL Friendly
-	$relevance = str_replace('%', '%252525', $relevance);
+	$relevance = str_replace('%', '%252525', $relevance); // This is the reason why I could not simply use "urlencode".  In order for '%' to be used in the relevance as a string you must specially encode it like this.
 	$relevance = str_replace(' ', '%20', $relevance);
 	$relevance = str_replace('!', '%21', $relevance);
 	$relevance = str_replace('"', '%22', $relevance);
